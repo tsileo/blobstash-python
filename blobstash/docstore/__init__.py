e6d65666260f1cc74d88d721264c5ff1a3ab135d
Thomas Sileo <t@a4.io>
2017-08-06 20:02:11 +0200

    Split up in multiple submodule

--- blobstash/docstore/__init__.py
+++ blobstash/docstore/__init__.py
@@ -2,7 +2,10 @@ from urllib.parse import urljoin
 from datetime import datetime
 from datetime import timezone
 
-import re
+from blobstash.docstore.query import Path  # noqa: unused-import  # make it accessible
+from blobstash.docstore.query import LogicalOperator
+from blobstash.docstore.query import Not
+from blobstash.docstore.query import LuaScript
 
 import requests
 
@@ -92,96 +95,6 @@ class DocStoreCursor:
     def __str__(self):
         return self.__repr__()
 
-class LuaScript:
-    def __init__(self, script):
-        self.script = script
-
-
-class LogicalOperator:
-    def __init__(self, *args):
-        self.clauses = args
-
-    def __str__(self):
-        return ' {} '.format(self.OPERATOR).join([str(clause) for clause in self.clauses])
-
-
-class Or(LogicalOperator):
-    OPERATOR = 'or'
-
-
-class And(LogicalOperator):
-    OPERATOR = 'and'
-
-
-class Not:
-    def __init__(self, clause):
-        self.clause = clause
-
-    def __str__(self):
-        return 'not ({})'.format(str(self.clause))
-
-
-class Path:
-    def __init__(self, path):
-        self.path = path
-        self._index_regex = re.compile(r'\[\d(?!\d)\]')
-
-    def any(self, values):
-        return ' or '.join([
-            'doc.{} == {}'.format(self._fix_index(self.path), self._lua_repr(value)) for value in values
-        ])
-
-    def not_any(self, values):
-        return ' or '.join([
-            'doc.{} ~= {}'.format(self._fix_index(self.path), self._lua_repr(value)) for value in values
-        ])
-
-    def contains(self, other):
-        # FIXME(tsileo): implements in_list in BlobStash
-        return 'in_list(doc.{}, {})'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __eq__(self, other):
-        return 'doc.{} == {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __lt__(self, other):
-        return 'doc.{} < {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __le__(self, other):
-        return 'doc.{} <= {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __ne__(self, other):
-        return 'doc.{} ~= {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __ge__(self, other):
-        return 'doc.{} >= {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def __gt__(self, other):
-        return 'doc.{} > {}'.format(self._fix_index(self.path), self._lua_repr(other))
-
-    def _fix_index(self, path):
-        """Lua array indices start at 1, so fix this.
-
-            >>> _fix_index("key[0].another_key")
-            "key[1].another_key
-
-        """
-        return self._index_regex.sub(lambda x: '[{}]'.format(int(x.group(0)[1:-1]) + 1), path)
-
-    def _lua_repr(self, other):
-
-        if isinstance(other, bytes):
-            return repr(other.decode('utf-8'))
-        elif isinstance(other, bool):
-            if other:
-                return 'true'
-            return 'false'
-        elif isinstance(other, (str, float, int)):
-            return repr(other)
-        elif isinstance(other, type(None)):
-            return 'nil'
-        else:
-            raise ValueError('unsupported data type: {}'.format(type(other)))
-
 
 class DocStoreCollection:
     def __init__(self, client, name):
@@ -221,11 +134,24 @@ class DocStoreCollection:
         elif isinstance(query, (LogicalOperator, Not)):
             query = str(query)
 
-        resp = self._client._get('/api/docstore/'+self.name+'?query='+query+'&script='+script+'&cursor='+cursor+'&limit='+str(limit)).json()
+        resp = self._client._get(
+            '/api/docstore/'+self.name,
+            params=dict(
+                query=query,
+                script=script,
+                cursor=cursor,
+                limit=str(limit),
+            ),
+        ).json()
         return resp
 
     def query(self, query='', script='', limit=50, cursor=''):
-        return DocStoreCursor(self, query, self._query(query, script=script, limit=limit, cursor=cursor))
+        return DocStoreCursor(self, query, self._query(
+            query,
+            script=script,
+            limit=limit,
+            cursor=cursor,
+        ))
 
     def get(self, query='', script=''):
         iterator = self.query(query, script=script)
---
+++ blobstash/docstore/query.py
@@ -0,0 +1,93 @@
+"""Query utils."""
+import re
+
+
+class LuaScript:
+    def __init__(self, script):
+        self.script = script
+
+
+class LogicalOperator:
+    def __init__(self, *args):
+        self.clauses = args
+
+    def __str__(self):
+        return ' {} '.format(self.OPERATOR).join([str(clause) for clause in self.clauses])
+
+
+class Or(LogicalOperator):
+    OPERATOR = 'or'
+
+
+class And(LogicalOperator):
+    OPERATOR = 'and'
+
+
+class Not:
+    def __init__(self, clause):
+        self.clause = clause
+
+    def __str__(self):
+        return 'not ({})'.format(str(self.clause))
+
+
+class Path:
+    def __init__(self, path):
+        self.path = path
+        self._index_regex = re.compile(r'\[\d(?!\d)\]')
+
+    def any(self, values):
+        return ' or '.join([
+            'doc.{} == {}'.format(self._fix_index(self.path), self._lua_repr(value)) for value in values
+        ])
+
+    def not_any(self, values):
+        return ' or '.join([
+            'doc.{} ~= {}'.format(self._fix_index(self.path), self._lua_repr(value)) for value in values
+        ])
+
+    def contains(self, other):
+        # FIXME(tsileo): implements in_list in BlobStash
+        return 'in_list(doc.{}, {})'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __eq__(self, other):
+        return 'doc.{} == {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __lt__(self, other):
+        return 'doc.{} < {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __le__(self, other):
+        return 'doc.{} <= {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __ne__(self, other):
+        return 'doc.{} ~= {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __ge__(self, other):
+        return 'doc.{} >= {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def __gt__(self, other):
+        return 'doc.{} > {}'.format(self._fix_index(self.path), self._lua_repr(other))
+
+    def _fix_index(self, path):
+        """Lua array indices start at 1, so fix this.
+
+            >>> _fix_index("key[0].another_key")
+            "key[1].another_key
+
+        """
+        return self._index_regex.sub(lambda x: '[{}]'.format(int(x.group(0)[1:-1]) + 1), path)
+
+    def _lua_repr(self, other):
+
+        if isinstance(other, bytes):
+            return repr(other.decode('utf-8'))
+        elif isinstance(other, bool):
+            if other:
+                return 'true'
+            return 'false'
+        elif isinstance(other, (str, float, int)):
+            return repr(other)
+        elif isinstance(other, type(None)):
+            return 'nil'
+        else:
+            raise ValueError('unsupported data type: {}'.format(type(other)))
