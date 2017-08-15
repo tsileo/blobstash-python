1eb774908a94c09a424a005d9c5c60b42711e19c
Thomas Sileo <t@a4.io>
2017-08-15 14:20:58 +0200

    Add tests

--- test_blobstash_docstore.py
+++ test_blobstash_docstore.py
@@ -1,5 +1,5 @@
 from blobstash.base.test_utils import BlobStash
-from blobstash.docstore import DocStoreClient
+from blobstash.docstore import DocStoreClient, Q
 
 
 def test_docstore():
@@ -8,6 +8,41 @@ def test_docstore():
     try:
         b.run()
         client = DocStoreClient(api_key='123')
+
+        col1 = client.col1
+
+        for i in range(10):
+            col1.insert({'lol': i+1})
+
+        col2 = client.col2
+
+        COL = 'hello'
+        DOCS_COUNT = 1000
+        docs = []
+        for i in range(DOCS_COUNT):
+            doc = dict(hello=i)
+            resp = col2.insert(doc)
+            docs.append(doc)
+
+        for doc in docs:
+            rdoc = col2.get_by_id(doc['_id'])
+            assert rdoc == doc
+
+        rdocs = []
+        for rdoc in col2.query():
+            rdocs.append(rdoc)
+
+        assert rdocs == docs[::-1]
+
+        col3 = client.col3
+
+        for i in range(50):
+            col3.insert({'i': i, 'nested': {'i': i}, 'l': [True, i]})
+
+        assert len(list(col3.query(Q['nested']['i'] >= 25))) == 25
+
+        assert sorted(['col1', 'col2', 'col3']) == sorted([c.name for c in client.collections()])
+
     finally:
         b.shutdown()
         b.cleanup()
