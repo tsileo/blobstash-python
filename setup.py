826ac0b78e2702288d362e8232464ae83c42a15f
Thomas Sileo <t@a4.io>
2018-01-22 17:44:05 +0100

    Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='blobstash-docstore',
-    version='0.0.5',
+    version='0.0.6',
     description='BlobStash DocStore client',
     long_description=long_description,
     author='Thomas Sileo',
